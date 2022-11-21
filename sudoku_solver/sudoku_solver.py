09afc82df97a2b31ecf9efce334fb521bd6ac2ae
Dippudo <107745404+dippudo@users.noreply.github.com>
2022-11-21 21:22:58 +1100

    Added functions to backtrack and validate guesses

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -1,4 +1,5 @@
 # random board generator: https://www.sudokuweb.org/
+# tutorial part 1: https://www.youtube.com/watch?v=eqUwSA0xI-s
 
 
 # row = sub-list, column = item in sub-list
@@ -24,7 +25,6 @@ def print_board(board):
         # print separator before board
         if row == 0:
             print("+" + "-" * 29 + "+")
-
         # print separator every third row
         if row % 3 == 0 and row != 0:
             print("+" + "-" * 29 + "+")
@@ -56,3 +56,49 @@ def find_empty(board):
         for column in range(len(board[row])):
             if board[row][column] == 0:
                 return (row, column)
+
+    return None
+
+
+def valid(board, number, position):
+    # check row
+    for i in range(len(board[0])): # loop through rows (we are getting the length of a row through len(board[0]))
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    # check column
+    for i in range(len(board)): # loop through columns (length of column == len(2dArray board))
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    # check 3x3 box
+    box_row = position[1] // 3 # column index // 3
+    box_column = position[0] // 3 # row index // 3
+
+    for row in range(box_column * 3, box_column * 3 + 3):
+        for column in range(box_row * 3, box_row * 3 + 3):
+            if board[row][column] == number and (row, column) != position:
+                return False
+
+    return True
+
+
+def solve(board): # updates board to the solved board
+    find = find_empty(board)
+
+    if not find: # if program cannot find an empty square
+        return True # solve = True / THE BOARD HAS BEEN SOLVED
+
+    else:
+        row, column = find # return the coordinates of the empty square, if found
+
+    for i in range(1, 10): # try every number between 1-9 inclusive
+        if valid(board, i, (row, column)):
+            board[row][column] = i # assign i to current empty square
+
+            if solve(board): # recursively try to solve the board, using the above valid() function call
+                return True
+
+            board[row][column] = 0 # reset the previously solved square to 0 if no answers are found
+
+    return False
